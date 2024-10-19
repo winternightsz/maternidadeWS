f15a63b2c46a655560aba14671fd0bcd4fdf7862
Izabela <104799350+winternightsz@users.noreply.github.com>
2024-10-18 21:49:13 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # conexão local - MySQL
-engine = create_engine("mysql+mysqldb://root:@localhost:3306/maternidade")
+# engine = create_engine("mysql+mysqldb://root:@localhost:3306/maternidade")
 
 # conexão supabase - PostgreSQL
-#engine = create_engine("postgresql://postgres:password@db.project.supabase.co:5432/postgres")
+engine = create_engine("postgresql://postgres.qciapfqtidaykoapeiqt:passwordToDatabase@@aws-0-sa-east-1.pooler.supabase.com:6543/postgres")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  
 Base = declarative_base()
 
